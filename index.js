e81eb0b4242014580ca376735b068ed10d7e78fc
anzhelikami <87754588+anzhelikami@users.noreply.github.com>
2021-10-04 13:12:30 +0300

    update index.js to new sdk

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ async function main()
 {
   const app = await dasha.deploy("./app");
 
-  app.connectionProvider = async (conv) =>
-    conv.input.phone === "chat"
-      ? dasha.chat.connect(await dasha.chat.createConsoleChat())
-      : dasha.sip.connect(new dasha.sip.Endpoint("default"));
-
-  app.ttsDispatcher = () => "dasha";
 
   app.setExternal("console_log", (args, conv) => 
   {
@@ -22,7 +16,13 @@ async function main()
 
   const conv = app.createConversation({ phone: process.argv[2] ?? "", name: process.argv[3] ?? "" });
 
-  if (conv.input.phone !== "chat") conv.on("transcription", console.log);
+    conv.audio.tts = "dasha";
+
+  if (conv.input.phone === "chat") {
+    await dasha.chat.createConsoleChat(conv);
+  } else {
+    conv.on("transcription", console.log);
+  }
 
   const logFile = await fs.promises.open("./log.txt", "w");
   await logFile.appendFile("#".repeat(100) + "\n");
@@ -38,7 +38,9 @@ async function main()
     }
   });
 
-  const result = await conv.execute();
+    const result = await conv.execute({
+    channel: conv.input.phone === "chat" ? "text" : "audio",
+  });
 
   console.log(result.output);
 
